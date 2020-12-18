fc02a25cfa96f88a42715df2df070208d7b10a8c
sannajoensen <76228761+sannajoensen@users.noreply.github.com>
2020-12-18 21:59:48 +0100

    Comments added

--- newtest run.py
+++ newtest run.py
@@ -1,11 +1,12 @@
+# Imports
 import pandas as pd
 import urllib.request
 from collections import Counter
 
 from newtest import recent_post_links, insta_link_details, insta_url_to_img
 
-#Change post count to change amount of post downloaded
-example_username_urls = recent_post_links('lilmiquela', post_count=30)
+
+example_username_urls = recent_post_links('lilmiquela', post_count=30) #Post count kan ændres i relation til antal ønskede posts
 print(example_username_urls)
 example_username_details = [insta_link_details(url) for url in example_username_urls]
 example_username = pd.DataFrame(example_username_details)
