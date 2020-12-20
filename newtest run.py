85c1502127296ff6dc0beb77ca0d33d07d29eb60
sannajoensen <76228761+sannajoensen@users.noreply.github.com>
2020-12-20 15:05:33 +0100

    New modification

--- newtest run.py
+++ newtest run.py
@@ -13,3 +13,7 @@ example_username = pd.DataFrame(example_username_details)
 example_username.head()
 csv = example_username.to_csv('/Users/Sanna/Desktop/Python/Instagram Scraper/example_username.csv')
 print(csv)
+
+# Koden er fundet på: 
+# jnawjux, 2020. Found on: https://github.com/jnawjux/web_scraping_corgis?fbclid=IwAR0AUgc_J5is-CtG2Pk7DWaqfix36paSxhgAewLxDm-69awxGV3cGZ82zUs [Sidst besøgt: 20.12.2020].
+# Python Simplified, 2020: https://www.youtube.com/watch?v=iJGvYBH9mcY [Sidst besøgt: 20.12.2020].
